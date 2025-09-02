8bce805beeefaf064d5ebd4ecdc2caa630e586f4
nanaios <game.ryd.131@gmail.com>
2025-09-02 23:22:04 +0900

    サーバーとクライアントで動作したっぴ

--- src/main/java/com/nanaios/curiosable_ammo_box/mixin/MixinAbstractGunItem.java
+++ src/main/java/com/nanaios/curiosable_ammo_box/mixin/MixinAbstractGunItem.java
@@ -9,12 +9,12 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(value = AbstractGunItem.class,remap = false)
 public class MixinAbstractGunItem {
-    @ModifyVariable(method = "lambda$canReload$1", at = @At("HEAD"),ordinal = 1, argsOnly = true)
+    @ModifyVariable(method = "lambda$canReload$1", at = @At("HEAD"), argsOnly = true)
     private static IItemHandler mixinAbstractGunItem$lambda$canReload$1(IItemHandler itemHandler) {
         return PlayerInvWrapperWithCurios.create(itemHandler);
     }
     @ModifyVariable(method = "findAndExtractInventoryAmmo",at=@At("HEAD"),argsOnly = true)
-    private static IItemHandler MixinAbstractGunItem$findAndExtractInventoryAmmo(IItemHandler itemHandler) {
+    private IItemHandler MixinAbstractGunItem$findAndExtractInventoryAmmo(IItemHandler itemHandler) {
         return PlayerInvWrapperWithCurios.create(itemHandler);
     }
 }
