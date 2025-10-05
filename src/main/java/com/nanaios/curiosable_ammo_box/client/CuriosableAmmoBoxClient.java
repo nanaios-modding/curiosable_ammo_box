35afb7bf500fa5960cb0c1dcb7ca3a00e72b34ec
nanaios <game.ryd.131@gmail.com>
2025-10-05 19:27:03 +0900

    レンダリングの実装を開始

---
+++ src/main/java/com/nanaios/curiosable_ammo_box/client/CuriosableAmmoBoxClient.java
@@ -0,0 +1,20 @@
+package com.nanaios.curiosable_ammo_box.client;
+
+import com.nanaios.curiosable_ammo_box.CuriosableAmmoBox;
+import com.nanaios.curiosable_ammo_box.client.renderer.CuriosableAmmoBoxCurioRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT, modid = CuriosableAmmoBox.MODID)
+public class CuriosableAmmoBoxClient {
+    public CuriosableAmmoBoxClient(final IEventBus eventBus) {
+        eventBus.addListener(this::clientSetup);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent evt) {
+        CuriosRendererRegistry.register(CurioModItems.MY_ITEM, () -> new CuriosableAmmoBoxCurioRenderer());
+    }
+}
