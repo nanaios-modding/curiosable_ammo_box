10dd1de80f7f7c03c199a19366f7adce6c1db89f
nanaios <game.ryd.131@gmail.com>
2026-05-31 00:05:10 +0900

    tacz1.1.8での動作を確認

--- src/main/java/com/nanaios/curios_for_ammo_box/CuriosForAmmoBox.java
+++ src/main/java/com/nanaios/curios_for_ammo_box/CuriosForAmmoBox.java
@@ -1,10 +1,15 @@
 package com.nanaios.curios_for_ammo_box;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Mod(CuriosForAmmoBox.MODID)
 public class CuriosForAmmoBox {
     public static final String MODID = "curios_for_ammo_box";
-    public CuriosForAmmoBox() {}
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public CuriosForAmmoBox() {
+    }
 }
